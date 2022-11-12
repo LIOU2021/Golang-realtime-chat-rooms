10430fdbb83731bc17d37d21997c3aff8f046485
LIOU <aa78789898tw@gmail.com>
2022-11-12 08:17:26 +0000

    add comment

--- README.md
+++ README.md
@@ -4,3 +4,8 @@
 `go run *.go`
  
 http://localhost:8080/room/1
+
+
+## todo
+- 重構 client, hub ，獨立出package來
+- [為connect 增加ID](https://stackoverflow.com/questions/64631848/how-to-create-an-autoincrement-id-field)
--- client.go
+++ client.go
@@ -40,16 +40,18 @@ type connection struct {
 // readPump pumps messages from the websocket connection to the hub.
 func (s subscription) readPump() {
 	c := s.conn
+
 	defer func() {
-		h.unregister <- s
+		h.unregister <- s //通知hub有人離開連線了，並且將該user的連線資訊傳送過去
 		c.ws.Close()
 	}()
+
 	c.ws.SetReadLimit(maxMessageSize)
 	c.ws.SetReadDeadline(time.Now().Add(pongWait))
 	c.ws.SetPongHandler(func(string) error { c.ws.SetReadDeadline(time.Now().Add(pongWait)); return nil })
 	for {
-		_, msg, err := c.ws.ReadMessage()
-		if err != nil {
+		_, msg, err := c.ws.ReadMessage() //獲取訊息。這裡會堵塞。後面的code要等message接收到。
+		if err != nil {                   //如果user斷開連線就會觸發break離開此迴圈，間接觸發h.unregister
 			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway) {
 				log.Printf("error: %v", err)
 			}
@@ -76,15 +78,16 @@ func (s *subscription) writePump() {
 	}()
 	for {
 		select {
-		case message, ok := <-c.send:
-			if !ok {
-				c.write(websocket.CloseMessage, []byte{})
+		case message, ok := <-c.send: //從readPump > broadcast 會塞資料到 c.send 這個channel裡面
+			if !ok { //收到c.send這個channel已經關閉了，所以要關閉連線
+				c.write(websocket.CloseMessage, []byte{}) //通知ws 關閉連線
 				return
 			}
-			if err := c.write(websocket.TextMessage, message); err != nil {
+
+			if err := c.write(websocket.TextMessage, message); err != nil { //前端發來訊息，寫入ws > 觸發readPump > 觸發broadcast(推播)
 				return
 			}
-		case <-ticker.C:
+		case <-ticker.C: //心跳機制。每X秒去ping前端在線用戶
 			if err := c.write(websocket.PingMessage, []byte{}); err != nil {
 				return
 			}
@@ -103,6 +106,6 @@ func serveWs(w http.ResponseWriter, r *http.Request, roomId string) {
 	c := &connection{send: make(chan []byte, 256), ws: ws} //個人的連線
 	s := subscription{c, roomId}                           //個人欲訂閱的房間
 	h.register <- s                                        //註冊hub，有人加入了連線
-	go s.writePump()                                       //當前用戶發送訊息時
-	go s.readPump()                                        //接收其他用戶訊息時
+	go s.writePump()                                       //server 寫進websocket的訊息
+	go s.readPump()                                        //server 讀取前端發過來的訊息
 }
